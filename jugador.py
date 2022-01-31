a763638f54af3409112d0781106b1ecf6f0effe2
moth1995 <marcos-flores@live.com.ar>
2022-01-31 02:14:10 -0300

    fixing position function

--- jugador.py
+++ jugador.py
@@ -237,12 +237,39 @@ def get_fav_side(posiciones):
 def get_pos_reg(pos):
     return {
         'GK': 0,
+        'CB': 3,
+        'LB': 4,
+        'RB': 4,
+        'CDM': 5,
+        'LWB': 6,
+        'RWB': 6,
+        'CM': 7,
+        'LM': 8,
+        'RM': 8,
+        'CAM': 9,
+        'LW': 10,
+        'RW': 10,
+        'LF': 11,
+        'RF': 11,
+        'CF': 11,
+        'ST': 12   
     }.get(pos, 0)    # si no encontramos la posicion devolvemos 0 que es GK
 
 
 def get_pos(posicion,posiciones):
     equivalencias= {
         0: 0,
+        2: 1,
+        3: 2,
+        4: 3,
+        5: 4,
+        6: 5,
+        7: 6,
+        8: 7,
+        9: 8,
+        10: 9,
+        11: 10,
+        12: 11   
     }
     indice=equivalencias.get(posicion)
     posiciones[indice]=1
