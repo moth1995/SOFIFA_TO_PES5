524b82a229e33a19f8592b77ed778cf081c3f5f9
moth1995 <marcos-flores@live.com.ar>
2022-02-01 07:25:55 -0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @echo on
-pyinstaller "main.py" --name "SoFIFA to PES5_WE9_LE" --noconsole
+pyinstaller "main.py" --name "SoFIFA to PES5_WE9_LE" --noconsole  --version-file file_version_info.txt
 xcopy "mdb_template\" "dist\SoFIFA to PES5_WE9_LE\mdb_template\"
 pause
